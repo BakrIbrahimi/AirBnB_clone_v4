4194afd7b63c157284d317b29f4dedf50522a61a
Bakr Ibrahimi <bakribra5@gmail.com>
2023-11-05 17:10:12 +0100

    add task3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+/* Script that listen for changes on each INPUT checkbox tag */
+$('document').ready(function () {
+    const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+    $.get(url, function (res) {
+      if (res.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  
+    const amenities = {};
+    $('INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    });
+  });
+  
